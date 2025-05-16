fe6a2dbc71d9b34950a4c56ea6ec800549fd734d
j1-dev <aephyxen@gmail.com>
2025-05-16 08:46:22 +0200

    Fixed deployment bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    preact({
-      prerender: {
-        enabled: true,
-        renderTarget: '#app',
-        additionalPrerenderRoutes: ['/404'],
-        previewMiddlewareEnabled: true,
-        previewMiddlewareFallback: '/404',
-      },
-    }),
-    tailwindcss(),
-  ],
+  plugins: [preact(), tailwindcss()],
 });
